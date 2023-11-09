957108fc69d66e826dd0bc2a9fd89ace26769379
Luana Portezan <luanaportezan@gmail.com>
2023-11-09 17:51:54 -0300

    como tinha parado

--- 02-typescript/src/index.ts
+++ 02-typescript/src/index.ts
@@ -274,3 +274,27 @@ const stgArray = concatArray<string[]>(['Portezan', 'Ana'], ['Alice']);
 
 // console.log(numArray);
 // console.log(stgArray);
+
+/*------------------------ DECORATORS -------------------------*/
+
+function exibirNome(target: any) {
+  console.log(target);
+}
+
+@exibirNome
+class Funcionario {}
+
+@exibirNome
+class Quincas {}
+
+function apiVersion(version: string) {
+  return (target:any) => {
+    Object.assign(target.prototype, {__version: version});
+  }
+}
+
+@apiVersion('1.10')
+class Api {}
+
+const api = new Api();
+console.log(api.__version);
